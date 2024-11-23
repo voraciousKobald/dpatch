5b5ace5e274c62dda4227e03e02720b9cb1b5ed0
voraciousKobald <ccgmv1@gmail.com>
2024-11-23 10:56:22 +0000

    updated code to reflect latest API update

--- extension_examples/welcomemsg/extension.sh
+++ extension_examples/welcomemsg/extension.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+
 scan "root" ".bashrc";
 if [ "$hit" = 1 ]; then
+	extHEAD "welcome message"
 	import "welcome" "root/.bashrc"
-	echo -e "\e[32msuccessfully imported!\e[0m\n"
-else
-	echo -e "\e[31mThis distribution does not use bash, so this extension is incompatible.\e[0m\n"
 fi
